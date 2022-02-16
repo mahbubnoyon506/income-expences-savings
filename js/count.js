3f6e36bf589310d7d0aa513470b1b999dfeffc84
unknown <you@example.com>
2022-02-17 04:20:01 +0600

    All calculation done

--- js/count.js
+++ js/count.js
@@ -28,5 +28,48 @@ document.getElementById('calculation').addEventListener('click', function(){
       
 })
 
+document.getElementById('saving-calculation').addEventListener('click', function(){
+   const savingInput = document.getElementById('saving-input');
+   const savingValue = savingInput.value;
+   const saving = parseFloat(savingValue);
+
+   const incomeInput= document.getElementById('income-input'); //duplicate
+   const income = incomeInput.value;
+   const incomeValue = parseFloat(income);
+
+   const initialSave = incomeValue * saving;
+   const totalSaving = initialSave / 100;
+
+   const savingText = document.getElementById('save-amount');
+   const savingCalculate = savingText.innerText;
+   savingText.innerText = totalSaving;
+   
+////////////////// duplicate
+const foodInput = document.getElementById('food-input');
+const foodCost = foodInput.value;
+const foodCostValue = parseFloat(foodCost);
+
+const rentInput = document.getElementById('rent-input');
+const rentCost = rentInput.value;
+const rentCostValue = parseFloat(rentCost);
+
+const clothesInput = document.getElementById('clothes-input')
+const clothesCost = clothesInput.value;
+const clothesCostValue = parseFloat(clothesCost);
+
+const countingExpense = document.getElementById('total-expense');
+const expense = countingExpense.innerText;
+countingExpense.innerText = parseFloat(expense);
+countingExpense.innerText = foodCostValue + rentCostValue + clothesCostValue;
+   const balanceCounting = document.getElementById('balance'); //duplicate
+   const balanceText = balanceCounting.innerText;
+   balanceCounting.innerText = parseFloat(balanceText);
+   balanceCounting.innerText = incomeValue - countingExpense.innerText;
+
+   const remainBalanceCost = document.getElementById('remain-balance');
+   const remainBalance = remainBalanceCost.innerText;
+   remainBalanceCost.innerText = balanceCounting.innerText - savingText.innerText;
+   console.log(remainBalanceCost.innerText);
+})
 
 
